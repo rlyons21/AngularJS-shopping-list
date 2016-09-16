ae29fc7066760b20fa8f12531866dcfd850ea3b6
Rachel Lyons <rlyons21@hotmail.com>
2016-09-15 19:49:21 -0500

    can't add blank item

--- controller.js
+++ controller.js
@@ -1,8 +1,15 @@
 var app = angular.module("myShoppingList", []); 
 app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.errortext = "";
     $scope.addItem = function () {
-        $scope.products.push($scope.addMe);
+    	if(!$scope.addMe) {
+    		return;
+    	}
+        if($scope.addMe.length > 0) {
+        	$scope.errortext = "";
+        	$scope.products.push($scope.addMe);
+        }
     }
     $scope.deleteItem = function ($index) {
     	$scope.products.splice($index,1);
